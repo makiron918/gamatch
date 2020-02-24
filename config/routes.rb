a2cbb31da7f668d9c868caa5398cbb8f4a8696f7
makiron918 <gonzaress918@gmail.com>
2020-02-24 15:11:02 +0900

    Delete comment controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
   resources :tops, only: :index
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] 
 end
