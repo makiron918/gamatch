38e89b693af9caad0fe1095e854a571dc437108e
makiron918 <gonzaress918@gmail.com>
2020-03-26 15:11:30 +0900

    Delete game.name
    gameテーブルのnameカラムを削除したため、関連する記述を削除した。

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,12 +4,7 @@ class Game < ApplicationRecord
   has_ancestry
   validates_associated :users
 
-  validates :name, presence: true
   validates :platform, presence: true
-
-  def new_or_record_changed
-    new_record? || will_save_change_to_nickname? || will_save_change_to_email? || will_save_change_to_image? || will_save_change_to_age? || will_save_change_to_sex? || will_save_change_to_voice? || will_save_change_to_intro?
-  end
 
   scope :id_is, -> id {
     where(id: id)
