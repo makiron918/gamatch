d1c0f92095e8d331e0d3a2d1efeeea7b4383753b
makiron918 <gonzaress918@gmail.com>
2020-04-02 13:05:52 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,15 +30,15 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc ‘upload master.key’
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
-      if test “[ ! -d #{shared_path}/config ]”
-        execute “mkdir -p #{shared_path}/config”
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
       end
-      upload!(‘config/master.key’, “#{shared_path}/config/master.key”)
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
-  before :starting, ‘deploy:upload’
-  after :finishing, ‘deploy:cleanup’
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
